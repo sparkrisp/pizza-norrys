40a1871026dea06781f09774e9bcb0074faff154
sparkrisp <arielrm@gmail.com>
2019-03-10 10:16:48 -0300

    zi zi ! 5

--- src/pages/ordering/cart/cart.ts
+++ src/pages/ordering/cart/cart.ts
@@ -36,6 +36,7 @@ export class CartPage {
   }
 
   mediaMasCart(item): any {
+   alert(item.product.category_id);
     if (this.mediaMas) {
       this.cart.setItemCount(item.product, -2);
     }else{
